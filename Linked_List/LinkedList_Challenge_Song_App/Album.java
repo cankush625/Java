b9a1b009224d964c0122ef8754dfff5bda8c77f1
cankush625 <cankush625@gmail.com>
2019-05-20 12:43:21 +0530

    Linked List challenge create songs app program added

---
+++ Linked_List/LinkedList_Challenge_Song_App/Album.java
@@ -0,0 +1,56 @@
+package com.Linked_List.LinkedList_Challenge_Song_App;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean addSong(String title, double duration) {
+        if (findSong(title) == null){
+            this.songs.add(new Song(title, duration));
+            return true;
+        }
+        return false;
+    }
+
+    private Song findSong(String title) {
+        for (int i = 0; i < this.songs.size(); i++) {
+            Song checkedSong = this.songs.get(i);
+            if (checkedSong.getTitle().equals(title)) {
+                return checkedSong;
+            }
+        }
+        return null;
+    }
+
+    public boolean addToPlaylist(int trackNumber, LinkedList<Song> playList) { //by using track number
+        int index = trackNumber - 1;
+        if ((index > 0) && (index <= this.songs.size())) {
+            playList.add(this.songs.get(index));
+            return true;
+        }
+        System.out.println("This album does not have a track " + trackNumber);
+        return false;
+    }
+
+    public boolean addToPlaylist(String title, LinkedList<Song> playlist) {
+        Song checkedSong = findSong(title);
+        if (checkedSong != null) {
+            playlist.add(checkedSong);
+            return true;
+        }
+        System.out.println("The song " + title + " is not in the album");
+        return false;
+    }
+}
+
+//by Ankush Chavan
---
+++ Linked_List/LinkedList_Challenge_Song_App/Song.java
@@ -0,0 +1,20 @@
+package com.Linked_List.LinkedList_Challenge_Song_App;
+
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + " : " + this.duration;
+    }
+}
