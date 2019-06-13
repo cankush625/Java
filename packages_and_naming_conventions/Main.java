814dd0b9754c5762838c34bff3ffdca9e481ed44
cankush625 <cankush625@gmail.com>
2019-06-13 16:58:15 +0530

    Packages and naming conventions are added

---
+++ packages_and_naming_conventions/Main.java
@@ -0,0 +1,8 @@
+package com.packages_and_naming_conventions;
+
+public class Main {
+    public static void main(String[] args) {
+        MyWindow mywindow = new MyWindow("Ankush Chavan");
+        mywindow.setVisible(true);
+    }
+}
---
+++ packages_and_naming_conventions/MyWindow.java
@@ -0,0 +1,29 @@
+package com.packages_and_naming_conventions;
+
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MyWindow extends Frame {
+    public MyWindow(String title) {
+        super(title);
+        setSize(500, 140);
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Font sansSerifLarge = new Font("SansSerif", Font.BOLD, 18);
+        Font sansSerifSmall = new Font("SansSerif", Font.BOLD, 12);
+        g.setFont(sansSerifLarge);
+        g.drawString("Hi Im Ankush Chavan", 60, 60);
+        g.setFont(sansSerifSmall);
+        g.drawString("Ethical Hacker \n Web Developer", 60, 100);
+    }
+}
