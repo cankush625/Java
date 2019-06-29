8221f68391211868c890bcfbf662bced34244043
cankush625 <cankush625@gmail.com>
2019-06-29 16:58:31 +0530

    Scope check is updated

--- packages_and_naming_conventions/scope_check/Main.java
+++ packages_and_naming_conventions/scope_check/Main.java
@@ -2,11 +2,11 @@ package com.packages_and_naming_conventions.scope_check;
 
 public class Main {
     public static void main(String[] args) {
-        String privateVar = "This is the private to main()";
+        String varFour = "This is the private to main()";
 
         ScopeCheck scopeInstance = new ScopeCheck();
-        System.out.println("ScopeInstance privateVar is " + scopeInstance.getPrivateVar());
-        System.out.println(privateVar);
+        System.out.println("ScopeInstance varOne is " + scopeInstance.getVarOne());
+        System.out.println(varFour);
 
         scopeInstance.timesTwo();
 
@@ -14,5 +14,9 @@ public class Main {
 
         ScopeCheck.InnerClass innerClassInstance = scopeInstance.new InnerClass();
         innerClassInstance.timesThree();
+
+        System.out.println("************************************");
+
+        scopeInstance.useInner();
     }
 }
--- packages_and_naming_conventions/scope_check/ScopeCheck.java
+++ packages_and_naming_conventions/scope_check/ScopeCheck.java
@@ -2,41 +2,47 @@ package com.packages_and_naming_conventions.scope_check;
 
 public class ScopeCheck {
     public int publicVar = 0;
-    private int privateVar = 1;
+    private int varOne = 1;
 
     public ScopeCheck() {
-        System.out.println("ScopeCheck created, publicVar = " + publicVar + " : privateVar = " + privateVar);
+        System.out.println("ScopeCheck created, publicVar = " + publicVar + " : varThree = " + varOne);
     }
 
-    public int getPrivateVar() {
-        return privateVar;
+    public int getVarOne() {
+        return varOne;
     }
 
     public void timesTwo() {
-        int privateVar = 2;
+        int varTwo = 2;
 
         for (int i = 0; i < 11; i++) {
-            System.out.println(i + " times 2 is " + i * privateVar);
+            System.out.println(i + " times 2 is " + i * varTwo);
         }
 
         //System.out.println("The value of i is now " + i); /*This gives an error for i because it is out of the scope*/
 
         for (int i = 0; i < 11; i++) {
-            System.out.println(i + " times 2 is " + i * this.privateVar); /*If we want to use the global variable then we are specifying that variable in the code followed by this keyword*/
+            System.out.println(i + " times 2 is " + i * this.varOne); /*If we want to use the global variable then we are specifying that variable in the code followed by this keyword*/
         }
     }
 
+    public void useInner() {
+        InnerClass innerClass = new InnerClass();
+        System.out.println("VarThree from outer class is " + innerClass.varThree);
+    }
+
     public class InnerClass {
-        public int privateVar = 3;
+        public int varThree = 3;
 
         public InnerClass() {
-            System.out.println("Inner class is created, privateVar is " + privateVar);
+            System.out.println("Inner class is created, varThree is " + varThree);
         }
 
         public void timesThree() {
+            System.out.println("VarOne is still available here " + varOne);
             ScopeCheck.this.timesTwo(); //Adding method in Scopecheck class using this keyword
             for (int i = 0; i < 11; i++) {
-                System.out.println(i + " times 3 is " + i * privateVar);
+                System.out.println(i + " times 3 is " + i * varThree);
             }
         }
     }
